0a0d7102963ea54296d91c5461063664c512de24
uniqual <daniel.popusoi@outlook.com>
2018-06-26 11:52:07 +0300

    Postgres SQL

--- src/main/java/com/isd/leaveassistant/config/WebMvcConfig.java
+++ src/main/java/com/isd/leaveassistant/config/WebMvcConfig.java
@@ -2,8 +2,6 @@ package com.isd.leaveassistant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Role;
-import org.springframework.security.authorization.AuthorityReactiveAuthorizationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
