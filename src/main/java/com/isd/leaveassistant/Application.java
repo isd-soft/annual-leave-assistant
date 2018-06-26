e948ac02e0a5d207a7b7c502c0581c044af5c4b7
uniqual <daniel.popusoi@outlook.com>
2018-06-26 15:23:25 +0300

    implement SQL command in roloes

--- src/main/java/com/isd/leaveassistant/Application.java
+++ src/main/java/com/isd/leaveassistant/Application.java
@@ -1,12 +1,32 @@
 package com.isd.leaveassistant;
 
+import com.isd.leaveassistant.model.Role;
+import com.isd.leaveassistant.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(Application.class, args);
+    }
+
+    private RoleRepository roleRepository;
+
+    public Application(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role roleAdmin = roleRepository.findByRole("ADMIN");
+        Role roleUser = roleRepository.findByRole("USER");
+
+        if (roleAdmin == null && roleUser == null) {
+            roleRepository.save(new Role("ADMIN"));
+            roleRepository.save(new Role("USER"));
+        }
     }
 }
