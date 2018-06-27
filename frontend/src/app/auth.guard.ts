90af080048325b2aceef31572ea7013dfe85b839
insunc <insunck@gmail.com>
2018-06-27 14:20:52 +0300

    renamed

---
+++ frontend/src/app/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+	private router: Router;
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      if(localStorage.getItem(environment.userToken) != null) {
+    		return true;
+    	} else { 
+    		this.router.navigate(['/login']);
+    		return false;
+      }
+  }
+}
---
+++ frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      if(localStorage.getItem(environment.userToken)) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+}
