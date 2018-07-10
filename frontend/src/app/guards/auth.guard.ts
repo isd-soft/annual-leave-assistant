e769e3f69d5a47b38a105f4c912e2ef7a7a4fbe7
vitalielazari <vitalie.lazari@gmail.com>
2018-07-10 09:28:00 +0200

    bug fixed

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if(localStorage.getItem('Token')) {
+      if(localStorage.getItem("token")) {
         return true;
       } else {
         return false;
