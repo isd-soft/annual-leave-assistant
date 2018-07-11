ed34617e36a8bfb5d0f3a4d46c048487f0d6f310
vitalielazari <vitalie.lazari@gmail.com>
2018-07-11 15:33:13 +0200

    deleted AngularDemo and Spring Demo

--- SpringDemo/src/main/java/com/example/demo2/repositoryDao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo2.repositoryDao;
-
-import com.example.demo2.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-    User findByEmail(String email);
-}
